b355463170c6c04c6118157be51116937c2d61c8
Ben Dudley <bendudley101@outlook.com>
2025-04-11 11:20:02 +0100

    Delete src/com/shipping/domain/carriers directory

--- src/com/shipping/domain/carriers/shipment/NoShipCapacityException.java
+++
@@ -1,7 +0,0 @@
-package com.shipping.domain.carriers.shipment;
-
-public class NoShipCapacityException extends Exception {
-    public NoShipCapacityException(String message) {
-        super(message);
-    }
-}
